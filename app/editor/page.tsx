4c4da39812d0ecc8152e5c9a34452defb4bb6615
StickyCoolDev <hujifa.info@gmaill.com>
2025-11-09 19:00:11 +0600

    Implement toogle full screen button

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef, Ref } from "react";
 import {
   ReactFlow,
   applyNodeChanges,
@@ -16,44 +16,64 @@ import "@xyflow/react/dist/style.css";
 const initialNodes = [
   {
     id: "n1",
-    position: { 
+    position: {
       x: 0,
-      y: 0 
+      y: 0,
     },
-    sourcePosition: Position.Right, 
-    data: { 
-      label: "AI"
-    } 
+    sourcePosition: Position.Right,
+    data: {
+      label: "AI",
+    },
   },
   { id: "n2", position: { x: 0, y: 100 }, data: { label: "Tool" } },
 ];
 const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
 
-
-type NodeChangeT = NodeChange<{ id: string; position: { x: number; y: number; }; data: { label: string; }; }>
-type EdgeChangeT = EdgeChange<{ id: string; source: string; target: string; }>
+type NodeChangeT = NodeChange<{
+  id: string;
+  position: { x: number; y: number };
+  data: { label: string };
+}>;
+type EdgeChangeT = EdgeChange<{ id: string; source: string; target: string }>;
 
 export default function Page() {
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
 
   const onNodesChange = useCallback(
-    (changes : NodeChangeT[]) =>
-      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot,)),
+    (changes: NodeChangeT[]) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
     [],
   );
   const onEdgesChange = useCallback(
-    (changes : EdgeChangeT[]) =>
-      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot,)),
+    (changes: EdgeChangeT[]) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
     [],
   );
   const onConnect = useCallback(
-    (params : any) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    (params: any) =>
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
     [],
   );
 
+  const MainContainerRef: null | Ref<HTMLDivElement> = useRef(null);
+
+  const toogleFullscreen = () => {
+    const element: HTMLDivElement | null = MainContainerRef.current;
+    if (element) {
+      if (document.fullscreenElement !== element) {
+        (element as HTMLDivElement).requestFullscreen();
+      } else {
+        document.exitFullscreen();
+      }
+    }
+  };
+
   return (
-    <div style={{ width: "95vw", height: "95vh" }}>
+    <div
+      ref={MainContainerRef as Ref<HTMLDivElement>}
+      style={{ width: "95vw", height: "95vh" }}
+    >
       <ReactFlow
         nodes={nodes}
         edges={edges}
